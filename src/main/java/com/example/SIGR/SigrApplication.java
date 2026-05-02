8dbac5eec24856a070250f7efe5cf1981b96efab
Ulrich008 <ulrich2.assogba@epitech.eu>
2026-05-02 14:55:50 +0100

    firt commit

---
+++ src/main/java/com/example/SIGR/SigrApplication.java
@@ -0,0 +1,13 @@
+package com.example.SIGR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SIGR/SigrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SIGR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
