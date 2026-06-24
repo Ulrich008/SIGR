8c3115cd2a4af0f9ef84f0bab1c84a5bdd5a5175
Ulrich008 <ulrich2.assogba@epitech.eu>
2026-06-24 17:22:49 +0100

    update project

--- src/main/java/com/example/SIGR/repository/PlanMitigationRepository.java
+++ src/main/java/com/example/SIGR/repository/PlanMitigationRepository.java
@@ -1,8 +1,10 @@
 package com.example.SIGR.repository;
 
 import com.example.SIGR.entity.PlanMitigation;
+import com.example.SIGR.entity.Risque;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PlanMitigationRepository extends JpaRepository<PlanMitigation, String> {
@@ -15,4 +17,6 @@ public interface PlanMitigationRepository extends JpaRepository<PlanMitigation, 
 
     boolean existsByLibelle(String libelle);
     boolean existsByLibelleAndCodeNot(String libelle, String code);
+    
+    List<PlanMitigation> findByRisque(Risque risque);
 }
